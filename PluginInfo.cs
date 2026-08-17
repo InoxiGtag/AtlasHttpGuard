2869134a4b67f9b885363ac144118ab0c32b62f3
Inoxi <195464490+InoxiGtag@users.noreply.github.com>
2026-08-17 07:54:58 +0200

    Rename plugin from RagdollMod-FixByInoxi to AtlasHttpGuard

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,7 +3,7 @@ namespace RagdollMod
     internal class PluginInfo
     {
         public const string GUID = "com.inoxiandgoldentrophy.gorillatag.ragdollmod";
-        public const string Name = "RagdollMod-FixByInoxi";
+        public const string Name = "AtlasHttpGuard";
         public const string Version = "1.0.0";
     }
 }
