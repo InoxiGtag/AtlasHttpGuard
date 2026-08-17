e82a0ab1f025d6ce2e4966e2c8460d1e17c593bc
Inoxi <195464490+InoxiGtag@users.noreply.github.com>
2026-08-17 07:56:31 +0200

    Update GUID for AtlasHttpGuard plugin

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@ namespace RagdollMod
 {
     internal class PluginInfo
     {
-        public const string GUID = "com.inoxiandgoldentrophy.gorillatag.ragdollmod";
+        public const string GUID = "com.inoxi.gtag.httpguard";
         public const string Name = "AtlasHttpGuard";
         public const string Version = "1.0.0";
     }
